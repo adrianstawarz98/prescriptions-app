821dea87734bd84bb20c6021e93a691c6e594a09
adrianstawarz98 <stwarz10233>
2022-05-26 17:37:38 +0200

    Initial commit

---
+++ src/main/java/com/thesis/prescriptionsapp/PrescriptionsAppApplication.java
@@ -0,0 +1,15 @@
+package com.thesis.prescriptionsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionsAppApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PrescriptionsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thesis/prescriptionsapp/PrescriptionsAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.thesis.prescriptionsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionsAppApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
