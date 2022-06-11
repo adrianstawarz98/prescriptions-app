3d07154c00ba3061743c2c8211e91ff39e32479d
adrianstawarz98@gmail.com <KlaudiaXDX123>
2022-06-11 17:48:37 +0200

    Replace springfox with springdoc

--- src/main/java/com/thesis/prescriptionsapp/controllers/PrescriptionsController.java
+++ src/main/java/com/thesis/prescriptionsapp/controllers/PrescriptionsController.java
@@ -6,23 +6,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 public class PrescriptionsController implements PrescriptionsApi {
     @Override
     public ResponseEntity<List<Prescription>> getAllPrescriptions() {
+        Date date = new Date();
+        OffsetDateTime offsetDateTime = date.toInstant().atOffset(ZoneOffset.UTC);
         List<Prescription> prescriptions = new ArrayList<>();
         Prescription prescription = new Prescription();
         prescription.setPrescriptionDoctor("Adrian");
         prescription.setPrescriptionUser("Damian");
-        prescription.setDatetime("2020-12-05 18:44:33");
+        prescription.setDatetime(offsetDateTime);
         prescription.setName("IBUM");
         Prescription prescription2 = new Prescription();
         prescription2.setPrescriptionDoctor("Adrian2");
         prescription2.setPrescriptionUser("Damian2");
-        prescription2.setDatetime("2020-12-05 18:44:33");
+        prescription2.setDatetime(offsetDateTime);
         prescription2.setName("IBUM2");
         prescriptions.add(prescription);
         prescriptions.add(prescription2);
